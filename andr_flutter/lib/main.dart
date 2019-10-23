3284caae5957e67654245c9979fc25ddab3c0fc4
MarshallJr <loisviaud@gmail.com>
2019-10-23 15:59:34 +0200

    Doc : BaseUrl

--- andr_flutter/lib/main.dart
+++ andr_flutter/lib/main.dart
@@ -10,6 +10,10 @@ void main() {
   ));
 }
 
+String baseUrl = "https://api.foursquare.com/v2/venues/search?client_id="+secret.clientId+"&client_secret="+secret.secretId+"&v=20180323&limit=1&ll=40.7243,-74.0018&near=Nantes";
+
+
+
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
   static const String _title = '♥ Kinder ♥';
@@ -53,7 +57,6 @@ class MyApp extends StatelessWidget {
               },
             ),
             TextFormField(
-
               scrollPadding: EdgeInsets.symmetric(vertical: 5.0),
               decoration: InputDecoration(
                 labelText: 'Entrez la localisation :',
@@ -113,71 +116,6 @@ class SecondRoute extends StatelessWidget {
             Navigator.pop(context);
           },
           child: Text('Go back!'),
-        ),
-      ),
-    );
-  }
-}
-
-
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  // The node used to request the keyboard focus.
-  final FocusNode _focusNode = FocusNode();
-// The message to display.
-  String _message;
-
-// Focus nodes need to be disposed.
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-// Handles the key events from the RawKeyboardListener and update the
-// _message.
-  void _handleKeyEvent(RawKeyEvent event) {
-    setState(() {
-      if (event.physicalKey == PhysicalKeyboardKey.keyA) {
-        _message = 'Pressed the key next to CAPS LOCK!';
-      } else {
-        _message = 'Wrong key.';
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final TextTheme textTheme = Theme.of(context).textTheme;
-    return Container(
-      color: Colors.white,
-      alignment: Alignment.center,
-      child: DefaultTextStyle(
-        style: textTheme.display1,
-        child: RawKeyboardListener(
-          focusNode: _focusNode,
-          onKey: _handleKeyEvent,
-          child: AnimatedBuilder(
-            animation: _focusNode,
-            builder: (BuildContext context, Widget child) {
-              if (!_focusNode.hasFocus) {
-                return GestureDetector(
-                  onTap: () {
-                    FocusScope.of(context).requestFocus(_focusNode);
-                  },
-                  child: Text('Tap to focus'),
-                );
-              }
-              return Text(_message ?? 'Press a key');
-            },
-          ),
         ),
       ),
     );
